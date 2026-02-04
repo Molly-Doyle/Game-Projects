6ed78d01242f157ffa5ea5418f966e920deeb03f
Molly-Doyle <N00254095@iadt.ie>
2026-02-04 16:07:14 +0000

    Spike trap and damage classwork

---
+++ Assets/SpikeTrap.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class spikeTrap : MonoBehaviour
+{
+    [SerializeField] float damage = 10f;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.ApplyDamage(damage);
+        }
+    }
+}
